0b0a9510641a6e527b7e347a19f79ffcea44f7e3
smartdev720 <successminato@gmail.com>
2024-03-24 21:23:10 -0400

    post buttons now work

--- components/CardTwitter.vue
+++ components/CardTwitter.vue
@@ -11,6 +11,13 @@ defineExpose({
     generate
 })
 
+const postURL = computed(
+    ()=> 
+        `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            announcement.value || ""
+        )}`
+);
+
 </script>
 <template>
     <CardGeneric
@@ -25,8 +32,8 @@ defineExpose({
                 <strong>{{ announcement?.length }}</strong>
             </div>
             <div>
-                <button class="btn btn-neutral">Regenerate</button>
-                <a class="btn btn-primary" targets="_blank">Post</a>
+                <button class="btn btn-neutral" @click="generate">Regenerate</button>
+                <a :href="postURL" class="btn btn-primary" targets="_blank">Post</a>
             </div>
         </div>
     </CardGeneric>
